f8ebad2511c8e8748fae24f4e09a8335ca27758f
OGSegu <seguisreal@gmail.com>
2021-04-08 01:43:32 +0300

    Added first test

--- src/test/java/template/controller/TemplateControllerTest.java
+++ src/test/java/template/controller/TemplateControllerTest.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.Assert;
+import template.entity.Template;
+import template.exception.TemplateNotFoundException;
+import template.repository.TemplateRepository;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -14,8 +18,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TemplateControllerTest {
 
+    // Repo
+    @Autowired
+    private TemplateRepository templateRepository;
+
+
     // URLS
     private final String LOAD_URL = "/api/template/load";
+
+    private final String TEMPLATE_ID = "internshipRequest";
+    private final String TEMPLATE_MSG = "Jetbrains Internship in $teamName$ team";
+    private final String ENDPOINT_FIRST = "http://localhost:8080/endpoint/first";
+    private final String ENDPOINT_SECOND = "http://localhost:8080/endpoint/second";
+
 
     @Autowired
     MockMvc mvc;
@@ -24,13 +39,16 @@ class TemplateControllerTest {
     void addTemplate() throws Exception {
         mvc.perform(post(LOAD_URL)
                 .contentType("application/json")
-                .content("{\n" +
-                        "\"templateId\": \"internshipRequest\",\n" +
-                        "\"template\": \"Jetbrains Internship in $teamName$ team and my name is $name$\",\n" +
-                        "\"recipients\": [\"http://localhost:8080/endpoint/first\", \"http://localhost:8080/endpoint/second\"]\n" +
-                        "}"))
+                .content(String.format("{\n" +
+                        "\"templateId\": \"%s\",\n" +
+                        "\"template\": \"%s\",\n" +
+                        "\"recipients\": [\"%s\", \"%s\"]\n" +
+                        "}", TEMPLATE_ID, TEMPLATE_MSG, ENDPOINT_FIRST, ENDPOINT_SECOND)))
                 .andDo(print())
                 .andExpect(status().isOk());
+        Template template;
+        Assert.isInstanceOf(Template.class, template = templateRepository.findById(TEMPLATE_ID).orElseThrow(TemplateNotFoundException::new));
+        Assert.hasText(TEMPLATE_MSG, template.getTemplateMsg());
     }
 
 }
